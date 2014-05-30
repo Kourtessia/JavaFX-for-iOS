7adb9e2eb67bda6604466b61ec23593a1f77dd18
Ania Kourtessi <ania@kourtessia.com>
2014-05-30 14:44:25 +0200

    JavaFX (OpenJFX) AddressBook app for iOS using RoboVM

---
+++ src/singletonshowcase/logic/AddressBookItem.java
@@ -0,0 +1,64 @@
+package com.asmierciak.designpatterns.singletonshowcase.logic;
+
+public class AddressBookItem {
+    private String firstName = "";
+    private String lastName = "";
+    private String fullAddress = "";
+//
+//    private String phoneNumber = "";
+//    private String emailAddress = "";
+
+    public AddressBookItem(String firstName, String lastName,
+            String fullAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullAddress = fullAddress;
+//        this.phoneNumber = phoneNumber;
+//        this.emailAddress = emailAddress;
+    }
+
+    public String toString() {
+        return String.format("%s %s %s", firstName, lastName,
+                fullAddress);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullAddress() {
+        return fullAddress;
+    }
+
+    public void setFullAddress(String fullAddress) {
+        this.fullAddress = fullAddress;
+    }
+//
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getEmailAddress() {
+//        return emailAddress;
+//    }
+//
+//    public void setEmailAddress(String emailAddress) {
+//        this.emailAddress = emailAddress;
+//    }
+}
